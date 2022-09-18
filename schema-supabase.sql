1b156afc9a607839f7feca386c408c3f88e110b4
pierre pracht <pierre.pracht@gmail.com>
2022-09-18 18:00:07 +0200

    policy update sur maison

--- schema-supabase.sql
+++ schema-supabase.sql
@@ -7,3 +7,9 @@ CREATE POLICY "Enable insert for authenticated users only" ON "public"."Maison"
 AS PERMISSIVE FOR INSERT
 TO authenticated
 WITH CHECK (true)
+
+CREATE POLICY "Enable update for authenticated users only" ON "public"."Maison"
+AS PERMISSIVE FOR UPDATE
+TO authenticated
+USING (true)
+WITH CHECK (true)
