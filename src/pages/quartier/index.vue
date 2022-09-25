f0dc45354198b6b45ddd480c3f81c24a8e66dddf
pierre pracht <pierre.pracht@gmail.com>
2022-09-25 12:56:55 +0200

    simple liste quartier

---
+++ src/pages/quartier/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { supabase } from "../../supabase";
+const { data, error } = await supabase.from("quartiercommune").select("*");
+if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
+</script>
+
+<template>
+  <section class="flex flex-col">
+    <h3 class="text-2xl">Liste des quartiers</h3>
+    <ul>
+      <li v-for="quartierObject in (data as any[])">
+        {{ quartierObject.libelle_Commune }} -
+        {{ quartierObject.libelle_Quartier }}
+      </li>
+    </ul>
+  </section>
+</template>
